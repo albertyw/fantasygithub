db15c674f15a2c01acc8c282b7dee6ad3e0e2de3
Albert Wang <albertyw@mit.edu>
2013-02-03 06:52:39 -0500

    Added RequestContext to manage and play

--- fantasygithub/view.py
+++ fantasygithub/view.py
@@ -4,17 +4,20 @@ from django.http import Http404
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from fantasygithub.models import Team
+from django.template import RequestContext
 
 @login_required
 def manage(request):
     if request.method == 'POST':
+        #if 'POST' in request.POST
         pass
     teams = []
     for team in Team.objects.filter(manager=request.user).all():
         teams += team.name
-    return render_to_response('views/manage.html', {'teams': []})
+    print teams
+    return render_to_response('views/manage.html', {'teams': teams}, context_instance=RequestContext(request),)
 
 @login_required
 def play(request):
-    return render_to_response('views/play.html')
+    return render_to_response('views/play.html', context_instance=RequestContext(request))
 
