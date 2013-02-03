60be7fd07f4b83b35135792b8c90d9bcd8f71a9f
Albert Wang <albertyw@mit.edu>
2013-02-03 01:19:00 -0500

    Corrected models

--- fantasygithub/models/models.py
+++ fantasygithub/models/models.py
@@ -8,23 +8,32 @@ class Manager(User):
     # first_name
     # last_name
     pass
+    class Meta:
+        app_label = 'fantasygithub'
     
 class Team(models.Model):
     manager = models.ForeignKey(Manager)
     name = models.CharField(max_length=64)
+    class Meta:
+        app_label = 'fantasygithub'
     
 class Dev(models.Model):
     teams = models.ManyToManyField(Team)
     login = models.CharField(max_length=64, primary_key=True)
     avatar_url = models.CharField(max_length=256)
     email = models.EmailField()
+    class Meta:
+        app_label = 'fantasygithub'
     
 class Commit(models.Model):
     dev = models.ForeignKey(Dev)
     time = models.TimeField()
     sha = models.CharField(max_length=40)
+    class Meta:
+        app_label = 'fantasygithub'
     
 class GitCache(models.Model):
     dev = models.ForeignKey(Dev)
     last_updated = models.TimeField(auto_now = True)
-    
+    class Meta:
+        app_label = 'fantasygithub'
