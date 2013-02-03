42d02b930c2044a1f7e354ea30eda7fe0f1ac4d3
Albert Wang <albertyw@mit.edu>
2013-02-03 07:44:41 -0500

    Added ability for manage to create and delete teams

--- fantasygithub/view.py
+++ fantasygithub/view.py
@@ -6,19 +6,33 @@ from django.contrib.auth.decorators import login_required
 from fantasygithub.models import Team
 from fantasygithub.github import GitInfo
 from django.template import RequestContext
+import json
 
 @login_required
 def manage(request):
     if request.method == 'POST':
         if 'create_team' in request.POST:
-            a = Team()
-            a.manager = request.user
-            a.name = request.POST['name']
-            a.save()
+            team = Team()
+            team.manager = request.user
+            team.name = request.POST['name']
+            team.save()
+            
+            git = GetInfo()
+            dev_usernames = json.loads(request.POST['devs'])
+            for dev_username in dev_usernames:
+                if git.is_dev(dev_username):
+                    dev = git.get_dev(dev_username)
+                    dev.teams.add(team)
+                    dev.save()
             return HttpResponse('done')
         if 'check_dev' in request.POST:
             a = GitInfo()
             return HttpResponse(str(a.is_dev(request.POST['username'])))
+        if 'delete_team' in request.POST:
+            team_name = request.POST['name']
+            team = Team.objects.filter(name=team_name, manager=request.user)
+            if len(team) != 0:
+                team.delete()
         
         
     teams = []
