da6f61c21bd13900675078415eaf331fc2443543
Albert Wang <albertyw@mit.edu>
2013-02-03 06:17:45 -0500

    Added user login backend

--- fantasygithub/public.py
+++ fantasygithub/public.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, UserCreationForm
@@ -8,9 +9,23 @@ def home(request):
     return render_to_response('views/index.html')
 
 def login(request):
+    errors = ''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('fantasygithub.view.manage'))
+            else:
+                errors = 'Disabled login'
+        else:
+            errors = 'Invalid login'
     form = AuthenticationForm(request)
     return render_to_response('views/login.html', 
-        {'form': form},
+        {'form': form,
+         'errors': errors},
         )
 
 def about(request):
