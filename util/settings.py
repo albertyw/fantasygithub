3b7888616389fb89f91e95a9424137cd6537786d
Albert Wang <albertyw@mit.edu>
2013-02-02 23:15:23 -0500

    Fixed static files

--- util/settings.py
+++ util/settings.py
@@ -54,7 +54,7 @@ MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://www.fantasygithub.com/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -64,13 +64,14 @@ STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://www.fantasygithub.com/static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'static/datepicker'),
 )
 
 # List of finder classes that know how to find static files in
@@ -122,7 +123,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'fantasygithub',
-    'bootstrap_toolkit'
 )
 
 # A sample logging configuration. The only tangible logging
