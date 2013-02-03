673de68e75ef6a15cc7d34dc544919c72536df9b
Albert Wang <albertyw@mit.edu>
2013-02-03 06:19:50 -0500

    Require login on manage and play

--- fantasygithub/view.py
+++ fantasygithub/view.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.contrib.auth import authenticate
 
+@login_required
 def manage(request):
     return render_to_response('views/manage.html', {'teams': []})
 
+@login_required
 def play(request):
     return render_to_response('views/play.html')
 
