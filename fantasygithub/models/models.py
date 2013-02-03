d0666d223e042356a5e7af3351605b987bfaedd2
Harry Schwartz <hrs@ginkgobioworks.com>
2013-02-03 04:26:07 -0500

    adding gravatars

--- fantasygithub/models/models.py
+++ fantasygithub/models/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import urllib, hashlib
 
 class Manager(User):
     # username
@@ -10,28 +11,32 @@ class Manager(User):
     pass
     class Meta:
         app_label = 'fantasygithub'
-    
+
 class Team(models.Model):
     manager = models.ForeignKey(Manager)
     name = models.CharField(max_length=64)
     class Meta:
         app_label = 'fantasygithub'
-    
+
 class Dev(models.Model):
     teams = models.ManyToManyField(Team)
     login = models.CharField(max_length=64, primary_key=True)
     avatar_url = models.CharField(max_length=256)
     email = models.EmailField()
+    def gravatar(self, size=75):
+        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(self.email.lower()).hexdigest()
+        return gravatar_url + '?' + urllib.urlencode({'s':str(size)})
+
     class Meta:
         app_label = 'fantasygithub'
-    
+
 class Commit(models.Model):
     dev = models.ForeignKey(Dev)
     time = models.TimeField()
     sha = models.CharField(max_length=40)
     class Meta:
         app_label = 'fantasygithub'
-    
+
 class GitCache(models.Model):
     dev = models.ForeignKey(Dev)
     last_updated = models.TimeField(auto_now = True)
