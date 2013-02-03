cc346b35b8cd705ee54a28464ebb81a97ca07662
Albert Wang <albertyw@mit.edu>
2013-02-02 21:26:54 -0500

    Added syncdb.sh

---
+++ syncdb.sh
@@ -0,0 +1,21 @@
+# Check the models
+echo 'Validating models'
+echo ''
+python manage.py validate
+OUT=$?
+if [ $OUT -ne 0 ];then
+   exit
+fi
+
+# Clear the database
+echo ''
+echo 'CLEARING DATABASE'
+echo ''
+python manage.py sqlclear statsonice | python manage.py dbshell
+
+# Make the database
+echo ''
+echo 'CREATING DATABASE'
+echo ''
+python manage.py syncdb
+
